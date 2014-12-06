6bc7506610904bb7a3e5ccb835ee308356b1875e
roncabal <Ron@Rons-MacBook-Pro.local>
2014-12-06 20:07:38 +0800

    Added view

---
+++ application/views/game/index.php
@@ -0,0 +1,22 @@
+<div id="appHolder">
+	<div class="sidebar pull-right">
+		<div class="title"><p>History</p></div>
+		<div class="history">
+			
+		</div>
+		<div class="title"><p>Text Editor</p></div>
+		<div class="text-editor-holder">
+			<textarea class="text-editor" autocapitalize="off" autocorrect="off" autocomplete="off" spellcheck="false"></textarea>
+		</div>
+		<div class="text-editor-buttons-holder">
+			<div class="buttons-holder">
+				<button>Run</button>
+				<button>Stop</button>
+				<button>Clear</button>
+			</div>
+		</div>
+	</div>
+	<div class="game-holder pull-right">
+
+	</div>
+</duv>
--- application/views/layout/front.php
+++ application/views/layout/front.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title><?php echo $title; ?></title>
-	</head>
-<h1>front layout</h1>
+<head>
+	<title>Instruct Me!</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/meyers-reset.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/animate.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/commons.css" />
+	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php echo $this->load->view($view,$param); ?>
+</body>
 </html>
