0a5d2877f88b6496de5e41cfa6a71f2c438c71c2
brysalazar12 <brysalazar12@gmail.com>
2014-12-06 17:03:04 +0800

    set config in database.php db name, user and password

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'instructme';
+$db['default']['password'] = 'secret';
+$db['default']['database'] = 'instructme';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
