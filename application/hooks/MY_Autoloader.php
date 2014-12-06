b884b8e29d0605cae3d11c97ba6bfc10b4388add
brysalazar12 <brysalazar12@gmail.com>
2014-12-06 17:23:04 +0800

    add hook for autoloader for libraries base on classname and filename

---
+++ application/hooks/MY_Autoloader.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class MY_Autoloader {
+	private $_include_paths = array(); 
+
+	
+	public function register(array $paths = array()) { 
+		$this->_include_paths = $paths; 
+		spl_autoload_register(array($this, 'autoloader')); 
+	}	
+	
+	public function autoloader($class) {
+		foreach($this->_include_paths as $path) { 
+			$filepath = $path . $class . EXT; 
+			if(! class_exists($class, FALSE) AND is_file($filepath)) {
+				include_once($filepath); break;
+			}
+		}
+	}	
+}
