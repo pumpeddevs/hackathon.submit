c64f454a19fc618525c2a0827a4c21f758a14167
brysalazar12 <brysalazar12@gmail.com>
2014-12-06 17:32:03 +0800

    add auth controller for authentication

---
+++ application/controllers/auth.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of auth
+ *
+ * @author Pumpeddevs <pumpeddevs@gmail.com>
+ */
+class Auth extends FrontController{
+	//put your code here
+}
