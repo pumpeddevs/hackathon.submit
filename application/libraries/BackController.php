870404ba763a2b5bb06dcc3eb5cd31d125b01988
brysalazar12 <brysalazar12@gmail.com>
2014-12-06 17:30:55 +0800

    add BackController this is base class for backend controller

---
+++ application/libraries/BackController.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of BackController
+ *
+ * @author Pumpeddevs <pumpeddevs@gmail.com>
+ */
+class BackController extends BaseController{
+	public function __construct() {
+		parent::__construct();
+	}
+}
