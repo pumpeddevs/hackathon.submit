df68e15acdf9488cfc19cc256c48d596f87eb7e0
roncabal <Ron@Rons-MacBook-Pro.local>
2014-12-07 19:36:01 +0800

    Logout

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -19,13 +19,7 @@
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Settings <span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li class="divider"></li>
-						<li><a href="#">Separated link</a></li>
-						<li class="divider"></li>
-						<li><a href="#">One more separated link</a></li>
+						<li><a href="<?php echo base_url('home/logout'); ?>">Logout</a></li>
 					</ul>
 				</li>
 			</ul>
