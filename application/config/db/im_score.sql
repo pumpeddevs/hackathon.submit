a3fe0c043eb9dfc9e2714d5fe9af7262f89c8ad8
brysalazar12 <brysalazar12@gmail.com>
2014-12-07 14:41:04 +0800

    add user_id field

--- application/config/db/im_score.sql
+++ application/config/db/im_score.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 06, 2014 at 08:07 PM
+-- Generation Time: Dec 07, 2014 at 02:39 PM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.5
 
@@ -28,11 +28,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `im_score` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `level_id` int(11) NOT NULL,
   `score` int(11) NOT NULL,
   `line_count` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `im_score`
+--
+
+INSERT INTO `im_score` (`id`, `user_id`, `level_id`, `score`, `line_count`) VALUES
+(1, 1, 1, 20, 10);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
