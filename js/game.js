45771bea20f77a0e5f5fd99e889cf470c4121c71
Kenneth Sungcaya <ksungcaya@gmail.com>
2014-12-08 00:31:55 +0400

    disclaimer off ajax post

--- js/game.js
+++ js/game.js
@@ -19,6 +19,42 @@
 
 	$('.stage').on('click', function(event) {
 		event.preventDefault();
-		
+
+		var disclaimer = $('#dis-wrapper');
+
+		if (!disclaimer) {
+			return timerStart();
+		}
+
+		disclaimer.fadeIn('slow');
 	});
+
+	$('.start-stage').on('click', function(event) {
+		event.preventDefault();
+		
+		var disOff = $('#disclaimer-off'),
+				disclaimer = $('#dis-wrapper');
+
+		if (disclaimer) {
+			if (disOff && disOff.is(':checked')) {
+
+				$.post(baseUrl + 'game/disclaimer_off',
+					{'disclaimer_off' : disOff.val()},
+					function(data, textStatus, xhr) {
+
+					}, 'json'
+				);
+			}
+
+			disclaimer.fadeOut('slow');
+		}
+
+		timerStart();
+
+	});
+
+	function timerStart() {
+
+		console.log('timer starts now!');
+	}
 })();
