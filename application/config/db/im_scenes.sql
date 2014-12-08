884d7930b0e46c47600f566d3e2aa3231053c747
Kenneth Sungcaya <ksungcaya@gmail.com>
2014-12-08 07:16:13 +0400

    added record to scenes table

--- application/config/db/im_scenes.sql
+++ application/config/db/im_scenes.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.20)
 # Database: instructme
-# Generation Time: 2014-12-08 01:05:54 +0000
+# Generation Time: 2014-12-08 03:15:16 +0000
 # ************************************************************
 
 
@@ -23,6 +23,8 @@
 # Dump of table im_scenes
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `im_scenes`;
+
 CREATE TABLE `im_scenes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `stage` int(11) DEFAULT NULL,
@@ -34,6 +36,15 @@ CREATE TABLE `im_scenes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+LOCK TABLES `im_scenes` WRITE;
+/*!40000 ALTER TABLE `im_scenes` DISABLE KEYS */;
+
+INSERT INTO `im_scenes` (`id`, `stage`, `subject`, `intro`, `problem`, `answer`, `assets`)
+VALUES
+	(1,1,'Variables and Data Types','<p>Variables are containers used for storing data values.\nA variable can store different data types like\nnumbers, strings, boolean and more. For now we will focus on these three.</p>\n<p>A number can be a decimal or whole number\nA string or text is a series of characters.\nBooleans can only have two values which are true or false.</p>\n\n<strong>Variables can be declared like these:</strong>\n\n<pre class=\"prettyprint\">\nvar number   = 22;\nvar someText = \"A text\";\nvar isNew    = true;\n</pre>\n\nVariables are widely used in every programming language\nso it\\\'s important to know when and how to use them.\n\n<strong>Exercise:</strong>\n\n<p>Your mother asked you to buy a red apple to a store near\nfrom your house, she gave you a budget of 4 bucks. Now\nstore the values to its appropriate variable.</p>','var apple; // should be string\nvar isStoreNear;\nvar budget;',NULL,NULL);
+
+/*!40000 ALTER TABLE `im_scenes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
